1cd3601afb275551f5c2cc40b81772128d8e820d
Kravtsov Hryhorii <207325093+hrykr@users.noreply.github.com>
2025-06-10 15:26:43 +0300

    Enum Reflect 0.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Enum Reflection Rust
 
-[<img alt="github" src="https://img.shields.io/badge/github-hrykr%2Fenum__reflect-blue?logo=github&label=github&link=https%3A%2F%2Fgithub.com%2Fhrykr%2Fenum-reflect" height="20">](https://github.com/hrykr/enum-reflect)
+[<img alt="github" src="https://img.shields.io/badge/github-hrykr%2Fenum--reflect-blue?logo=github&label=github&link=https%3A%2F%2Fgithub.com%2Fhrykr%2Fenum-reflect" height="20">](https://github.com/hrykr/enum-reflect)
 [<img alt="crates.io" src="https://img.shields.io/crates/v/enum_reflect?logo=rust" height="20">](https://crates.io/crates/enum_reflect)
 [<img alt="crates.io" src="https://img.shields.io/docsrs/enum_reflect?logo=docs.rs&label=docs.rs" height="20">](https://docs.rs/enum_reflect/latest/enum_reflect/)
 
@@ -15,6 +15,11 @@ Rust library for enum reflection. Use it to get all fields in enum.
 [dependencies]
 enum_reflect = "0.1"
 ```
+
+## Dependencies
+
+> ### [Enum Reflect Extern](https://crates.io/crates/enum_reflect_extern)
+> Extern traits, structs and other for enum_reflect.
 
 ## Example
 
@@ -37,13 +42,13 @@ fn foo() {
     let exml = Example::Example1 { var1: "Hello, World!".to_string(), var2: 32 };
     
     for (name, value) in exml.get_named_fields() {
-        println!("Field {}.", name);
+        println!("Field {} is {}", name, value.to_string());
     }
 }
 
 ```
 > ### Output:
 > ```
-> Field var1.
-> Field var2.
+> Field var1 is Hello, World!
+> Field var2 is 32
 > ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident};
 
-/// Get fields names and value refs
+/// Derive reflection for enum fields to access name and value.
+/// Use mut functions to get mutable fields.
 /// 
 /// # Implementing
 /// 
@@ -12,10 +13,11 @@ use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident};
 /// ```
 /// # Functions
 /// 
-/// ```
-/// myenum.get_fields_mut() -> Vec<&mut dyn std::any::Any> /* Return mutable field values */
-/// myenum.get_named_fields_mut() -> Vec<(&'static str, &mut dyn std::any::Any)> /* Return field names and mutable field values */
-/// ```
+/// - `fn get_fields() -> Vec<&mut dyn std::any::Any>` Return immutable field values
+/// - `fn get_named_fields() -> Vec<(&'static str, &mut dyn std::any::Any)>` Return field names and immutable field values
+/// - `fn get_fields_mut() -> Vec<&mut dyn std::any::Any>` Return mutable field values
+/// - `fn get_named_fields_mut() -> Vec<(&'static str, &mut dyn std::any::Any)>` Return field names and mutable field values
+///
 /// # Example
 /// 
 /// ```
@@ -37,16 +39,16 @@ use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident};
 ///     let exml = Example::Example1 { var1: "Hello, World!".to_string(), var2: 32 };
 ///     
 ///     for (name, value) in exml.get_named_fields() {
-///         println!("Field {}.", name);
+///         println!("Field {} is {}", name, value.to_string());
 ///     }
 /// }
 /// ```
 /// 
 /// ## Output
-/// ```
-/// Field var1.
-/// Field var2.
-/// ```
+///
+/// - `Field var1 is Hello, World!`
+/// - `Field var2 is 32`
+///
 #[proc_macro_derive(EnumReflect)]
 pub fn enum_reflection(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -137,8 +139,87 @@ pub fn enum_reflection(input: TokenStream) -> TokenStream {
         }
     });
 
+    // For get_fields_mut()
+    let get_fields_mut_arms = data_enum.variants.iter().map(|v| {
+        let variant_ident = &v.ident;
+
+        match &v.fields {
+            Fields::Named(fields_named) => {
+                let bindings: Vec<_> = fields_named.named.iter().map(|f| {
+                    let ident = f.ident.as_ref().unwrap();
+                    quote! { #ident }
+                }).collect();
+
+                let refs: Vec<_> = bindings.iter().map(|ident| {
+                    quote! { #ident as &mut dyn std::any::Any }
+                }).collect();
+
+                quote! {
+                    #name::#variant_ident { #(#bindings),* } => vec![#(#refs),*]
+                }
+            }
+
+            Fields::Unnamed(fields_unnamed) => {
+                let bindings: Vec<_> = (0..fields_unnamed.unnamed.len())
+                    .map(|i| Ident::new(&format!("f{}", i), v.ident.span()))
+                    .map(|ident| quote! { #ident })
+                    .collect();
+
+                let refs: Vec<_> = bindings.iter().map(|ident| {
+                    quote! { #ident as &mut dyn std::any::Any }
+                }).collect();
+
+                quote! {
+                    #name::#variant_ident( #(#bindings),* ) => vec![#(#refs),*]
+                }
+            }
+
+            Fields::Unit => {
+                quote! {
+                    #name::#variant_ident => vec![]
+                }
+            }
+        }
+    });
+
+    // For get_named_fields_mut()
+    let get_named_fields_mut_arms = data_enum.variants.iter().map(|v| {
+        let variant_ident = &v.ident;
+
+        match &v.fields {
+            Fields::Named(fields_named) => {
+                let bindings: Vec<_> = fields_named.named.iter().map(|f| {
+                    let ident = f.ident.as_ref().unwrap();
+                    quote! { #ident }
+                }).collect();
+
+                let pairs: Vec<_> = fields_named.named.iter().map(|f| {
+                    let ident = f.ident.as_ref().unwrap();
+                    let name_str = ident.to_string();
+                    quote! { (#name_str, #ident as &mut dyn std::any::Any) }
+                }).collect();
+
+                quote! {
+                    #name::#variant_ident { #(#bindings),* } => vec![#(#pairs),*]
+                }
+            }
+
+            Fields::Unnamed(_) => {
+                quote! {
+                    #name::#variant_ident(..) => vec![]
+                }
+            }
+
+            Fields::Unit => {
+                quote! {
+                    #name::#variant_ident => vec![]
+                }
+            }
+        }
+    });
+
     let expanded = quote! {
-        impl #name {
+        impl enum_reflect_extetn::EnumReflect for #name {
             pub fn get_fields(&self) -> Vec<&dyn std::any::Any> {
                 match self {
                     #(#get_fields_arms)*
@@ -150,26 +231,25 @@ pub fn enum_reflection(input: TokenStream) -> TokenStream {
                     #(#get_named_fields_arms)*
                 }
             }
+
+            pub fn get_fields_mut(&mut self) -> Vec<&mut dyn std::any::Any> {
+                match self {
+                    #(#get_fields_mut_arms),*
+                }
+            }
+
+            pub fn get_named_fields_mut(&mut self) -> Vec<(&'static str, &mut dyn std::any::Any)> {
+                match self {
+                    #(#get_named_fields_mut_arms),*
+                }
+            }
         }
     };
 
     expanded.into()
 }
 
-/// Get field names and mutable values
-/// 
-/// # Implementing
-/// 
-/// ```
-/// #[derive(EnumReflectMut)]
-/// enum ExampleEnumMut;
-/// ```
-/// # Functions
-/// 
-/// ```
-/// myenum.get_fields_mut() -> Vec<&mut dyn std::any::Any> /* Return mutable field values */
-/// myenum.get_named_fields_mut() -> Vec<(&'static str, &mut dyn std::any::Any)> /* Return field names and mutable field values */
-/// ```
+#[deprecated(note = "Use #[derive(EnumReflect)] instead, now mut also included there")]
 #[proc_macro_derive(EnumReflectMut)]
 pub fn enum_reflection_mut(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
